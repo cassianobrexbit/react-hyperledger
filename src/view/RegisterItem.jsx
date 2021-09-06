a91739f787e21b6064393019e97be531bc64ca53
ErikHPO <opata@alunos.utfpr.edu.br>
2021-09-06 18:43:57 -0300

    change class to className

--- src/view/RegisterItem.jsx
+++ src/view/RegisterItem.jsx
@@ -20,32 +20,32 @@ const RegisterBudget = (props) => {
               
     <div className="container">
         <h2>Cadastrar Item</h2>
-    <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label  class="form-label">Item</label>
-    <input type="text" class="form-control" onChange={e=> setItem(e.target.value)} value={item} required/>
+    <form className="row g-3 needs-validation" noValidate>
+  <div className="col-md-4">
+    <label  className="form-label">Item</label>
+    <input type="text" className="form-control" onChange={e=> setItem(e.target.value)} value={item} required/>
   </div>
-  <div class="col-md-4">
-    <label  class="form-label">Referência</label>
-    <input type="text" class="form-control" onChange={e=> setRef(e.target.value)} value={ref} required/>
+  <div className="col-md-4">
+    <label  className="form-label">Referência</label>
+    <input type="text" className="form-control" onChange={e=> setRef(e.target.value)} value={ref} required/>
 
   </div>
-  <div class="col-md-4">
-    <label  class="form-label">Código</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
-      <input type="text" class="form-control" onChange={e=> setCod(e.target.value)} value={cod}   aria-describedby="inputGroupPrepend" required/>
+  <div className="col-md-4">
+    <label  className="form-label">Código</label>
+    <div className="input-group has-validation">
+      <span className="input-group-text" id="inputGroupPrepend">@</span>
+      <input type="text" className="form-control" onChange={e=> setCod(e.target.value)} value={cod}   aria-describedby="inputGroupPrepend" required/>
 
     </div>
   </div>
-  <div class="col-md-4">
-    <label  class="form-label">Discriminação</label>
-    <input type="text" class="form-control" onChange={e=> setDisc(e.target.value)} value={disc}   required/>
+  <div className="col-md-4">
+    <label  className="form-label">Discriminação</label>
+    <input type="text" className="form-control" onChange={e=> setDisc(e.target.value)} value={disc}   required/>
 
   </div>
-  <div class="col-md-2">
-    <label  class="form-label">Unidade Medida</label>
-    <select class="form-select"  onChange={e=> setUM(e.target.value)} value={uM}  required>
+  <div className="col-md-2">
+    <label  className="form-label">Unidade Medida</label>
+    <select className="form-select"  onChange={e=> setUM(e.target.value)} value={uM}  required>
       <option selected disabled value="">Selecione</option>
       <option>M²</option>
       <option>M³</option>
@@ -56,27 +56,27 @@ const RegisterBudget = (props) => {
     </select>
 
   </div>
-  <div class="col-md-2">
-    <label  class="form-label">Quantidade</label>
-    <input type="number" class="form-control"  onChange={e=> setQTD(e.target.value)} value={qtd}   required/>
+  <div className="col-md-2">
+    <label  className="form-label">Quantidade</label>
+    <input type="number" className="form-control"  onChange={e=> setQTD(e.target.value)} value={qtd}   required/>
 
   </div>
-  <div class="col-md-2">
-    <label  class="form-label">Preço Unit. com BDI</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text">R$</span>
-      <input type="number" step="0.01" class="form-control" id="validationCustomUsername" onChange={e => setValorUnit(e.target.value)} value={valorUnit}  aria-describedby="inputGroupPrepend"  required/>
+  <div className="col-md-2">
+    <label  className="form-label">Preço Unit. com BDI</label>
+    <div className="input-group has-validation">
+      <span className="input-group-text">R$</span>
+      <input type="number" step="0.01" className="form-control" id="validationCustomUsername" onChange={e => setValorUnit(e.target.value)} value={valorUnit}  aria-describedby="inputGroupPrepend"  required/>
     </div>
   </div>
-  <div class="col-md-2">
-    <label  class="form-label">Valor Total com BDI</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">R$</span>
-      <input step="0.01" class="form-control" id="validationCustomUsername"  aria-describedby="inputGroupPrepend" value={valorTotal} disabled/>
+  <div className="col-md-2">
+    <label  className="form-label">Valor Total com BDI</label>
+    <div className="input-group has-validation">
+      <span className="input-group-text" id="inputGroupPrepend">R$</span>
+      <input step="0.01" className="form-control" id="validationCustomUsername"  aria-describedby="inputGroupPrepend" value={valorTotal} disabled/>
     </div>
   </div>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Adicionar Item</button>
+  <div className="col-12">
+    <button className="btn btn-primary" type="submit">Adicionar Item</button>
   </div>
 </form>
     
